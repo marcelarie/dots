e190bf99657475bfb92030260fabae9338cf6f24
Marcel Arie <googlillo@gmail.com>
2022-02-19 16:17:15 +0100

    delete nix generated files

--- .config/fish/prenix-config.fish
+++ .config/fish/prenix-config.fish
@@ -1,39 +1,115 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    # atuin init fish | source
+# =============================================================================
+#
+# Utility functions for zoxide.
+#
+
+# pwd based on the value of _ZO_RESOLVE_SYMLINKS.
+function __zoxide_pwd
+    builtin pwd -L
 end
-# ≃≃≃≃≃≃≃≃≃≃≃≃≃≃≃ #
-#   colorscheme   #
-# ≃≃≃≃≃≃≃≃≃≃≃≃≃≃≃ ⩨
 
+# A copy of fish's internal cd function. This makes it possible to use
+# `alias cd=z` without causing an infinite loop.
+if ! builtin functions -q __zoxide_cd_internal
+    if builtin functions -q cd
+        builtin functions -c cd __zoxide_cd_internal
+    else
+        alias __zoxide_cd_internal='builtin cd'
+    end
+end
+
+# cd + custom logic based on the value of _ZO_ECHO.
+function __zoxide_cd
+    __zoxide_cd_internal $argv
+end
+
+# =============================================================================
+#
+# Hook configuration for zoxide.
+#
+
+# Initialize hook to add new entries to the database.
+function __zoxide_hook --on-variable PWD
+    test -z "$fish_private_mode"
+    and command zoxide add -- (__zoxide_pwd)
+end
+
+# =============================================================================
+#
+# When using zoxide with --no-aliases, alias these internal functions as
+# desired.
+#
+
+# Jump to a directory using only keywords.
+function __zoxide_z
+    set argc (count $argv)
+    if test $argc -eq 0
+        __zoxide_cd $HOME
+    else if test "$argv" = -
+        __zoxide_cd -
+    else if test $argc -eq 1 -a -d $argv[1]
+        __zoxide_cd $argv[1]
+    else
+        set -l result (command zoxide query --exclude (__zoxide_pwd) -- $argv)
+        and __zoxide_cd $result
+    end
+end
+
+# Completions for `z`.
+function __zoxide_z_complete
+    set -l tokens (commandline -op)
+    set -l curr_tokens (commandline -cop)
+
+    if test (count $tokens) -le 2 -a (count $curr_tokens) -eq 1
+        # If there is only one argument, use `cd` completions.
+        __fish_complete_directories "$tokens[2]" ''
+    else
+        # Otherwise, use interactive selection.
+        set -l query $tokens[2..-1]
+        set -l result (_ZO_FZF_OPTS='--bind=ctrl-z:ignore --exit-0 --height=35% --inline-info --no-sort --reverse --select-1' zoxide query -i -- $query)
+        and commandline -p "$tokens[1] "(string escape $result)
+        commandline -f repaint
+    end
+end
+
+# Jump to a directory using interactive search.
+function __zoxide_zi
+    set -l result (command zoxide query -i -- $argv)
+    and __zoxide_cd $result
+end
+
+# =============================================================================
+#
+# Convenient aliases for zoxide. Disable these using --no-aliases.
+#
+
+# Remove definitions.
+function __zoxide_unset
+    set --erase $argv >/dev/null 2>&1
+    abbr --erase $argv >/dev/null 2>&1
+    builtin functions --erase $argv >/dev/null 2>&1
+end
+
+__zoxide_unset z
+alias z=__zoxide_z
+complete -c z -e
+complete -c z -f -a '(__zoxide_z_complete)'
+
+__zoxide_unset zi
+alias zi=__zoxide_zi
+
+# =============================================================================
+#
+# To initialize zoxide, add this to your configuration (usually
+# ~/.config/fish/config.fish):
+#
+# zoxide init fish | source
+#
 # base16-gruvbox-dark-medium # :)
-
-# ≃≃≃≃≃≃≃≃≃≃≃≃≃≃≃ #
-#   ZSH  CONFIG   #
-# ≃≃≃≃≃≃≃≃≃≃≃≃≃≃≃ ⩨
 
 ### "bat" as manpager
 # export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 export MANPAGER='nvim +Man!'
-## go
-# set --universal -x PATH /home/$USER/.cargo/bin:$PATH # export GOPATH=$HOME/.local/go
-# set --universal -x GOPATH $HOME/.local/go # export PATH=/home/$USER/.cargo/bin:$PATH
-
-### "vim" as manpager
-# export MANPAGER='/bin/bash -c "vim -MRn -c \"set buftype=nofile showtabline=0 ft=man ts=8 nomod nolist norelativenumber nonu noma\" -c \"normal L\" -c \"nmap q :qa<CR>\"</dev/tty <(col -b)"'
-### "nvim" as manpager
-# export MANPAGER="nvim -c 'set ft=man' -"
-
-# ≃≃≃≃≃≃≃≃≃≃≃≃≃≃ #
-#   FISH ALIAS   #
-# ≃≃≃≃≃≃≃≃≃≃≃≃≃≃ ⩨
-
-# source $HOME/.config/fish/functions/br.fish
-source $HOME/.config/fish/git-alias.fish
-source $HOME/.config/fish/zoxide.fish
-source $HOME/.config/fish/functions/git.fish
-source $HOME/.config/fish/functions/nvim.fish
-source $HOME/.config/fish/functions/google.fish
 
 # ≃≃≃≃≃≃≃≃≃≃≃≃≃≃ #
 #  KEY-BINDINGS  #
@@ -47,51 +123,11 @@ bind -M insert \ce end-of-line # on vi-mode
 
 xset r rate 150 45
 set fish_greeting
-
-alias mon2cam="deno run --unstable -A -r -q https://raw.githubusercontent.com/ShayBox/Mon2Cam/master/src/mod.ts"
-alias luamake=/home/marcel/.config/nvim/language-servers/lua-language-server/3rd/luamake/luamake
-
-# ≃≃≃≃≃≃≃≃≃≃≃≃≃≃ #
-#    SOURCES     #
-# ≃≃≃≃≃≃≃≃≃≃≃≃≃≃ ⩨
-
-# set -x PERLBREW_ROOT /usr/local/soft/perlbrew/
-# bass source ~/perl5/perlbrew/etc/bashrc
-
-# ≃≃≃≃≃≃≃≃≃≃≃≃≃≃ #
-#  ENV.VARIABLES #
-# ≃≃≃≃≃≃≃≃≃≃≃≃≃≃ ⩨
-
-# vim
-# set VICONFIG /home/marcel/.config/nvim/init.lua
-# export VICONFIG
-#
-# # Ruby
-#
-# # Perl
-# # set PERL5LIB "/home/marcel/perl5/lib/perl5${PERL5LIB:+:${PERL5LIB}}"
-# set -x PERL5LIB /home/marcel/perl5/lib/perl5
-# export PERL5LIB
-#
-# # set PERL_LOCAL_LIB_ROOT "/home/marcel/perl5${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_ROOT}}"
-# set -x PERL_LOCAL_LIB_ROOT /home/marcel/perl5
-# export PERL_LOCAL_LIB_ROOT
-#
-# set -x NODE_PATH (npm root -g)
-# export NODE_PATH
-#
-# set PERL_MB_OPT --install_base \"/home/marcel/perl5\"
-# export PERL_MB_OPT
-#
-# set PERL_MM_OPT INSTALL_BASE=/home/marcel/perl5
-# export PERL_MM_OPT
-
-# SoySuper
+~/clones/forks/fetch-master-6000/fm6000.pl
 
 # /home/marcel/clones/own/git-tellme/target/release/git-tellme
 # fm6000
-~/clones/forks/fetch-master-6000/fm6000.pl
-starship init fish | source
 
 # Generated for envman. Do not edit.
 # test -s "$HOME/.config/envman/load.fish"; and source "$HOME/.config/envman/load.fish"
+starship init fish | source
