42e454c6e05a5b8b83395c2fe535b0b6f789c59d
Marcel Arie <googlillo@gmail.com>
2022-03-18 22:57:51 +0100

    update

---
+++ scripts/fish.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title fish
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+/etc/profiles/per-user/m.manzanares/bin/kitty fish
