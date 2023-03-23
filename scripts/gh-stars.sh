451bd63f9584c121c7119759cf708fe5d966eb67
Marcel Arie <googlillo@gmail.com>
2023-03-23 17:15:40 +0100

    updating script

--- scripts/gh-stars.sh
+++ scripts/gh-stars.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
-star=$(gh api user/starred --template '{{range .}}{{.full_name|color "yellow"}} ({{timeago .updated_at}}){{"\n"}}{{end}}' --paginate --cache 1h | fzf)
+star=$(
+	gh api user/starred --template \
+		'{{range .}}{{.full_name|color "yellow"}} ({{timeago .updated_at}}){{"\n"}}{{end}}' \
+		--paginate \
+		--cache 1h |
+		fzf
+)
 repo=$(echo "$star" | cut -d' ' -f1)
 
 if [[ -n $star ]]; then
-  gh repo view "$repo" -w
+	gh repo view "$repo" -w
 fi
