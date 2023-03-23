67cba812efd62ec5ed5da8e6124902b4c76d9708
Marcel Arie <googlillo@gmail.com>
2023-03-23 17:11:05 +0100

    feat: add gh stars script

---
+++ scripts/gh-stars.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+star=$(gh api user/starred --template '{{range .}}{{.full_name|color "yellow"}} ({{timeago .updated_at}}){{"\n"}}{{end}}' --paginate --cache 1h | fzf)
+repo=$(echo "$star" | cut -d' ' -f1)
+
+if [[ -n $star ]]; then
+  gh repo view "$repo" -w
+fi
