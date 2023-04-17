b0448fcfcd04534681ef8e4ff097666f8944a5c8
Marcel Arie <googlillo@gmail.com>
2023-04-17 09:31:27 +0200

    update

--- scripts/install-last-neovim.sh
+++ scripts/install-last-neovim.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 PROGRAMS="$HOME/programs"
+BUILD_NVIM="sudo make CMAKE_BUILD_TYPE=Release install && nvim -v"
 
 [ ! -d "$PROGRAMS" ] && mkdir "$PROGRAMS"
 [ ! -d "$PROGRAMS/neovim" ] && git clone https://github.com/neovim/neovim "$PROGRAMS/neovim"
 
 cd "$PROGRAMS/neovim/" || exit
 git pull --all --stat
-time sudo make CMAKE_BUILD_TYPE=Release install && nvim -v
+
+time (eval "$BUILD_NVIM") || (make diskclean && eval "$BUILD_NVIM")
