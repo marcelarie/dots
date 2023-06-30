341df10c72506e27e583491289ad91737b279dcb
Marcel Arie <googlillo@gmail.com>
2023-06-30 17:16:52 +0200

    update

--- scripts/tmux/create-session.sh
+++ scripts/tmux/create-session.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env sh
 
-WORK_PATH="$HOME/clones/work"
+SESSION_TYPE=$1
+
+if [ -z "$SESSION_TYPE" ]; then
+	echo "No session type provided"
+	exit 1
+fi
+
+WORK_PATH="$HOME/clones/$SESSION_TYPE"
 # Selecting a repository to work on
 SELECTED_REPO=$(ls "$WORK_PATH" | fzy)
+REPO=$(echo $SELECTED_REPO | sed 's/\//-/g' | sed 's/\./-dot-/g')
+SESSION="$SESSION_TYPE-$REPO"
 REPO_NAME="$WORK_PATH/$SELECTED_REPO/"
 # Generating the session name using the repository
-SESSION="work-$SELECTED_REPO"
 SHELL=fish
 EDITOR=nvim
 CUSTOM_COMMAND="fnm use"
