b528005593410bcca49d194eb7071184d719db29
marcelarie <googlillo@gmail.com>
2024-04-25 20:29:57 +0200

    improve tmux script

--- scripts/tmux/create-session-tmex.sh
+++ scripts/tmux/create-session-tmex.sh
@@ -10,7 +10,7 @@ fi
 WORK_PATH="$HOME/clones/$SESSION_TYPE"
 # Selecting a repository to work on
 SELECTED_REPO=$(ls "$WORK_PATH" | fzy)
-REPO=$(echo "$SELECTED_REPO" | sed 's/\//-/g' | sed 's/\./-dot-/g')
+REPO=$(echo $SELECTED_REPO | sed 's/\//-/g' | sed 's/\./-dot-/g')
 SESSION="$SESSION_TYPE-$REPO"
 REPO_NAME="$WORK_PATH/$SELECTED_REPO/"
 # Generating the session name using the repository
@@ -18,11 +18,6 @@ REPO_NAME="$WORK_PATH/$SELECTED_REPO/"
 EDITOR=nvim
 # CUSTOM_COMMAND="fnm use"
 
-if ! tmux has-session -t "$SESSION" 2>/dev/null; then
-	tmex "$SESSION" --layout="2{41}" "cd $REPO_NAME; clear; $EDITOR" "cd $REPO_NAME; clear"
-
-else
-	echo "Session already exists"
-fi
-
-tmux -2 attach-session -t "$SESSION"
+tmex "$SESSION" \
+	--reattach  \
+	--layout="2{41}" "cd $REPO_NAME; clear; $EDITOR" "cd $REPO_NAME; clear"
