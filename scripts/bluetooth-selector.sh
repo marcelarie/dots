a6b4393271ee5d646b42e5f0170bbd10d0ddf0a3
marcelarie <googlillo@gmail.com>
2024-09-19 16:45:27 +0200

    update bluetooth-selector script

--- scripts/bluetooth-selector.sh
+++ scripts/bluetooth-selector.sh
@@ -1,21 +1,57 @@
 #!/usr/bin/env bash
 
-devices="$(bluetoothctl devices)"
-device_names=$(echo "$devices" | cut --fields 3- --delimiter ' ')
-selected=$(echo "$device_names" | wofi --dmenu -i -p "Bluetooth devices" -no-custom)
+# Function to send notifications
+notify_user() {
+	local status=$1
+	local message=$2
+	local icon=$3
+	notify-send "$icon $message"
+}
 
-id=$(echo "$devices" | rg "$selected" | awk '{print $2}')
+mapfile -t devices < <(bluetoothctl devices)
+mapfile -t connected_device_names < <(bluetoothctl devices Connected | cut --fields 3- --delimiter ' ')
+mapfile -t all_device_names < <(printf '%s\n' "${devices[@]}" | cut --fields 3- --delimiter ' ')
 
-if [ -z "$selected" ]; then
-	exit 1
-fi
+connected_sign="✔️"
+disconnected_sign="❌"
 
-notify-send "Connecting to $selected device..."
+# Prepend a sign to each device name based on its connection status
+devices_result=""
+for device in "${all_device_names[@]}"; do
+	if [[ " ${connected_device_names[*]} " == *" $device "* ]]; then
+		devices_result+="${connected_sign} $device"$'\n'
+	else
+		devices_result+="${disconnected_sign} $device"$'\n'
+	fi
+done
 
-bluetoothctl connect $id
+selected=$(printf "%s" "$devices_result" | wofi --dmenu -i -p "Bluetooth devices" -no-custom | cut --fields=2- --delimiter=" ")
 
-if [ $? -eq 0 ]; then
-	notify-send "Connected to $selected device"
+[[ -z $selected ]] && exit 1
+
+device_id=$(printf '%s\n' "${devices[@]}" | awk -v dev="$selected" '$0 ~ dev {print $2}')
+
+[[ -z "$device_id" ]] && exit 1
+
+# Handle connections and notify the user
+manage_connection() {
+	local action=$1
+	local device=$2
+	local id=$3
+	local sign=$4
+
+	if bluetoothctl "$action" "$id"; then
+		notify_user "$action" "$device" "$sign"
+	else
+		notify_user "$action" "Failed to $action $device" "❌"
+	fi
+}
+
+# Check if the device is connected and toggle its status
+if printf "%s\n" "${connected_device_names[@]}" | grep -Fxq "$selected"; then
+	notify_user "disconnect" "Disconnecting from $selected..." ">"
+	manage_connection "disconnect" "$selected" "$device_id" "$disconnected_sign"
 else
-	notify-send -u critical "Failed to connect to $selected device"
+	notify_user "connect" "Connecting to $selected..." ">"
+	manage_connection "connect" "$selected" "$device_id" "$connected_sign"
 fi
